4e8c4ba3fd8852a3365e07642deefca51e0f6159
Nicolas308804 <nicogr300804@gmail.com>
2025-04-09 12:20:16 -0500

    Portafolio-belleza 3

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-        Portafolio Nicolas Gonzalez Master en React 2024
+        Portafolio Carol Andrea Rodriguez Rubio
     </footer>
   )
 }
